635ad206beeb032fff420363332cf8c103274212
thelensbender <ijinleifeshadare@gmail.com>
2025-05-19 15:11:53 +0100

    After assignment 2

---
+++ Solidity Practice/assignment2.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4; 
+contract personal {
+    string private name = "Ijinleifeoluwa";
+    string internal  location ="Osun";
+    uint private  age = 20;
+    address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint256 internal  balance = 10000000000000000000000000;
+}
