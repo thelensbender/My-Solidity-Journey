051a265d871c1c960cfbd667084a7995ca06807b
thelensbender <ijinleifeshadare@gmail.com>
2025-05-19 13:57:13 +0100

    m

--- Solidity Practice/person.sol
+++ Solidity Practice/person.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.4;
 
 contract Person {
     string name = "Ijinleifeoluwa";
-    uint  age = 20;
+    uint age = 20;
     address  tokenOwner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 }
