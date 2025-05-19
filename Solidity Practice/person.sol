c37750b25338f971d6ba8849d822953b89cc7edd
thelensbender <ijinleifeshadare@gmail.com>
2025-05-19 13:37:11 +0100

    First Push

---
+++ Solidity Practice/person.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Person {
+    string name = "Ijinleifeoluwa";
+    uint  age = 20;
+    address  tokenOwner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
